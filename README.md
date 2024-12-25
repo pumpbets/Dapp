0c97d77da27b84a23e1df354a22ea8e02fe47682
wikig <carzygodwu@gmail.com>
2024-12-25 05:54:22 +0000

    update readme

---
+++ README.md
@@ -0,0 +1,23 @@
+# PumpBets NEXTUI
+
+Pumpbets is first AI-Agent driven solana memecoin bets protocol . 
+
+Anyone can type a words or @me in social media to place a new bet about memecoin . 
+
+## What kinds of bet it support ?
+
+- Token/coins marketcap 
+
+- Will pumpcoin reach king of the hill ?
+
+- Price of token will up or down
+
+- Will mememcoin up to 2x 3x 5x 10x ?
+
+## How to place the bet ?
+
+- Visit our site and type some words about your bets , the AI agent will place bets for you .
+
+- @pumpbets in twitter , and tell AI what you bets . 
+
+- Visit our channel or telegram bot , to place bets in it.
---
+++ pages/_app.tsx
@@ -0,0 +1,43 @@
+'use client';
+import type { AppProps } from "next/app";
+
+import { NextUIProvider } from "@nextui-org/system";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { useRouter } from "next/router";
+
+import { fontSans, fontMono } from "@/config/fonts";
+import "@/styles/globals.css";
+
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { useMemo } from "react";
+
+const endpoint = 'https://api.devnet.solana.com';
+
+export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const wallets = useMemo(
+    () => [
+    ],
+    []
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+    <WalletProvider wallets={wallets} autoConnect>
+      <WalletModalProvider>
+      <NextUIProvider navigate={router.push}>
+          <NextThemesProvider attribute="class" defaultTheme="dark">
+            <Component {...pageProps} />
+          </NextThemesProvider>
+        </NextUIProvider>
+      </WalletModalProvider>
+    </WalletProvider>
+    </ConnectionProvider>
+  );
+}
+
+export const fonts = {
+  sans: fontSans.style.fontFamily,
+  mono: fontMono.style.fontFamily,
+};
