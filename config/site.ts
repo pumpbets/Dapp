a3322dc2c34a8858771a1b8876a0299a368baf50
wikig <carzygodwu@gmail.com>
2024-12-26 12:28:43 +0000

    final page

--- config/site.ts
+++ config/site.ts
@@ -12,7 +12,7 @@ export const siteConfig = {
     base:"https://pumpbets.fun/",
     redirect:"https://pumpbets.fun/api/redirect",
     github: "https://github.com/pumpbets",
-    twitter: "https://x.com/macondocrypto",
+    twitter: "https://x.com/pumpbetai",
     docs: "",
     discord: "https://discord.gg/9b6yyZKmH4",
     sponsor: "https://patreon.com/jrgarciadev",
